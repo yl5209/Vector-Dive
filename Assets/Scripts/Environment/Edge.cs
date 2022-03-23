dc9b8cb8efabbbc85de5f8515458b361e25b6e09
Alfie Luo <alfie.luo@gmail.com>
2022-03-23 16:39:36 -0400

    Edge fix

--- Assets/Scripts/Environment/Edge.cs
+++ Assets/Scripts/Environment/Edge.cs
@@ -12,11 +12,14 @@ public class Edge : MonoBehaviour
     public int density = 36;
     public float max_offset = 1f;
     [SerializeField]
-    [Range(1f, 5f)]
+    [Range(0.1f, 5f)]
     public float speed = 3f;
 
     private List<Vector3> Nodes_v3;
     private List<Vector2> Nodes_v2;
+
+    private List<Vector3> Nodes_v3_dynamic;
+    private List<Vector2> Nodes_v2_dynamic;
 
     private LineRenderer lineRenderer_static;
     private LineRenderer lineRenderer_dynamic;
@@ -31,8 +34,12 @@ public class Edge : MonoBehaviour
         lineRenderer_static = GetComponent<LineRenderer>();
         lineRenderer_dynamic = transform.GetChild(0).GetComponent<LineRenderer>();
         edgeCollider = GetComponent<EdgeCollider2D>();
+
         Nodes_v3 = new List<Vector3>();
         Nodes_v2 = new List<Vector2>();
+
+        Nodes_v3_dynamic = new List<Vector3>();
+        Nodes_v2_dynamic = new List<Vector2>();
 
         float angle = 360 / density;
 
@@ -40,15 +47,20 @@ public class Edge : MonoBehaviour
         {
             Nodes_v3.Add(new Vector3(Mathf.Cos(angle * Mathf.Deg2Rad * i) * radius, Mathf.Sin(angle * Mathf.Deg2Rad * i) * radius, 0f));
             Nodes_v2.Add(new Vector3(Mathf.Cos(angle * Mathf.Deg2Rad * i) * radius, Mathf.Sin(angle * Mathf.Deg2Rad * i) * radius));
+            Nodes_v3_dynamic.Add(new Vector3(Mathf.Cos(angle * Mathf.Deg2Rad * i) * radius, Mathf.Sin(angle * Mathf.Deg2Rad * i) * radius, 0f));
+            Nodes_v2_dynamic.Add(new Vector3(Mathf.Cos(angle * Mathf.Deg2Rad * i) * radius, Mathf.Sin(angle * Mathf.Deg2Rad * i) * radius));
         }
 
+        Nodes_v3.Add(Nodes_v3[0]);
         Nodes_v2.Add(Nodes_v2[0]);
-
+        Nodes_v3_dynamic.Add(Nodes_v3[0]);
+        Nodes_v2_dynamic.Add(Nodes_v3[0]);
+        
         lineRenderer_static.positionCount = density;
         lineRenderer_static.SetPositions(Nodes_v3.ToArray());
 
         lineRenderer_dynamic.positionCount = density;
-        lineRenderer_dynamic.SetPositions(Nodes_v3.ToArray());
+        lineRenderer_dynamic.SetPositions(Nodes_v3_dynamic.ToArray());
 
         edgeCollider.SetPoints(Nodes_v2);
     }
@@ -64,16 +76,21 @@ public class Edge : MonoBehaviour
         float angle = 360 / density;
         float length, target;
 
-        for (int i = 0; i < density; i++)
-        {
-            length = math.remap(0f, 0.21f, 0f, 1f, samples) * max_offset;
-            target = radius + length;
-            offset = Mathf.SmoothDamp(offset, target, ref vel, speed);
+        length = math.remap(0f, 0.21f, 0f, 1f, samples) * max_offset;
+        target = radius + length;
+        offset = Mathf.SmoothDamp(offset, target, ref vel, speed);
 
-            Nodes_v3[i] = new Vector3(Mathf.Cos(angle * Mathf.Deg2Rad * i) * offset, Mathf.Sin(angle * Mathf.Deg2Rad * i) * offset, 0f);
-            Nodes_v2[i] = new Vector3(Mathf.Cos(angle * Mathf.Deg2Rad * i) * offset, Mathf.Sin(angle * Mathf.Deg2Rad * i) * offset);
+        for (int i = 0; i <= density; i++)
+        {
+            Nodes_v3[i] = new Vector3(Mathf.Cos(angle * Mathf.Deg2Rad * i) * radius, Mathf.Sin(angle * Mathf.Deg2Rad * i) * radius, 0f);
+            Nodes_v2[i] = new Vector3(Mathf.Cos(angle * Mathf.Deg2Rad * i) * radius, Mathf.Sin(angle * Mathf.Deg2Rad * i) * radius);
+
+            Nodes_v3_dynamic[i] = new Vector3(Mathf.Cos(angle * Mathf.Deg2Rad * i) * offset, Mathf.Sin(angle * Mathf.Deg2Rad * i) * offset, 0f);
+            Nodes_v2_dynamic[i] = new Vector3(Mathf.Cos(angle * Mathf.Deg2Rad * i) * offset, Mathf.Sin(angle * Mathf.Deg2Rad * i) * offset);
         }
 
-        lineRenderer_dynamic.SetPositions(Nodes_v3.ToArray());
+        lineRenderer_static.SetPositions(Nodes_v3.ToArray());
+        lineRenderer_dynamic.SetPositions(Nodes_v3_dynamic.ToArray());
+        edgeCollider.SetPoints(Nodes_v2);
     }
 }
