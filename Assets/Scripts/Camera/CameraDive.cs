6b8f75880fe81ddf7717ca9bbc2d5b3e3389cfde
Alfie Luo <alfie.luo@gmail.com>
2022-04-03 12:59:20 -0400

    New VFX, Camera Dive Rework, Volume change

--- Assets/Scripts/Camera/CameraDive.cs
+++ Assets/Scripts/Camera/CameraDive.cs
@@ -7,6 +7,9 @@ public class CameraDive : MonoBehaviour
 {
 
     public float duration = 0.5f;
+    public float dive_depth = 1f;
+    public float dive_height = 300f;
+    public float fall_duration = 3f;
     public bool IsDiving { get; set; }
 
     private CameraFollow cameraFollow;
@@ -32,14 +35,14 @@ public class CameraDive : MonoBehaviour
     {
         cameraFollow.look_at = false;
         IsDiving = true;
-        dive = transform.DOMove(new Vector3(Player.instance.transform.position.x, Player.instance.transform.position.y, 1), duration).SetEase(Ease.InBack).Pause().SetAutoKill(false).OnComplete(() =>
+        dive = transform.DOMove(new Vector3(Player.instance.transform.position.x, Player.instance.transform.position.y, dive_depth), duration).SetEase(Ease.InBack).Pause().SetAutoKill(false).OnComplete(() =>
         {
 
-            transform.position = new Vector3(transform.position.x, transform.position.y, -300f);
+            transform.position = new Vector3(transform.position.x, transform.position.y, -dive_height);
             CameraManager.main_camera.DOColor(Random.ColorHSV(0.0f, 1.0f, 0.3f, 0.7f, 0.5f, 0.8f), duration * 2f);
             cameraFollow.look_at = true;
 
-            fall = transform.DOMove(new Vector3(Player.instance.transform.position.x, Player.instance.transform.position.y, -17f), duration).SetEase(Ease.OutCirc).Pause().SetAutoKill(false).OnComplete(() =>
+            fall = transform.DOMove(new Vector3(Player.instance.transform.position.x, Player.instance.transform.position.y, -17f), fall_duration).SetEase(Ease.OutCirc).Pause().SetAutoKill(false).OnComplete(() =>
             {
                 IsDiving = false;
                 //cameraFollow.ResetVelocity();
--- Assets/Scripts/Managers/AudioManager.cs
+++ Assets/Scripts/Managers/AudioManager.cs
@@ -33,22 +33,11 @@ public class AudioManager : MonoBehaviour
             AudioListener.GetSpectrumData(spectrum, 0, FFTWindow.Triangle);
             sample_time = Time.time + frequency;
         }
+    }
 
-        //for (int i = 1; i < spectrum.Length - 1; i++)
-        //{
-        //    Debug.DrawLine(new Vector3(i - 1, spectrum[i] * amplitude + 10, 0), new Vector3(i, spectrum[i + 1] * amplitude + 10, 0), Color.red);
-        //    Debug.DrawLine(new Vector3(i - 1, Mathf.Log(spectrum[i - 1]) + 10, 2), new Vector3(i, Mathf.Log(spectrum[i]) + 10, 2), Color.cyan);
-        //    Debug.DrawLine(new Vector3(Mathf.Log(i - 1), spectrum[i - 1] - 10, 1), new Vector3(Mathf.Log(i), spectrum[i] - 10, 1), Color.green);
-        //    Debug.DrawLine(new Vector3(Mathf.Log(i - 1), Mathf.Log(spectrum[i - 1]), 3), new Vector3(Mathf.Log(i), Mathf.Log(spectrum[i]), 3), Color.blue);
-        //}
-
-        if (Input.GetKeyDown(KeyCode.Q))
-        {
-            for(int i = 0; i < spectrum.Length; i++)
-            {
-                Debug.Log(spectrum[i]);
-            }
-        }
+    public void SetVolume(float v)
+    {
+        GetComponent<AudioSource>().volume = v;
     }
 
     public float[] GetSamples()
